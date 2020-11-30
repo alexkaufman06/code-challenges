46ba1e5a9cceefae3d2c4c256167f40830950279
alexkaufman06 <alexkaufman06@gmail.com>
2020-11-29 18:28:29 -0800

    Ignore dist directory

--- dist/src/reverse-words.js
+++
@@ -1,4 +0,0 @@
-export function reverseWords(str) {
-    return str.split(' ').map(function (word) { return word.split('').reverse().join(''); }).join(' ');
-}
-//# sourceMappingURL=reverse-words.js.map
