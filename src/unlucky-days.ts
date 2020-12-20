a41043117cc67e23afa82b163499b26ad3028c3e
alexkaufman06 <alexkaufman06@gmail.com>
2020-12-19 16:10:44 -0800

    Refactor to reduce length of function

--- src/unlucky-days.ts
+++ src/unlucky-days.ts
@@ -1,9 +1,7 @@
 export function unluckyDays(year: number): number {
     let count = 0;
     for (let x = 0; x < 12; x++) {
-        if (new Date(year, x, 13).getDay() == 5) {
-            count ++
-        }
+        count += new Date(year, x, 13).getDay() == 5 ? 1 : 0;
     }
     return count;
 }
