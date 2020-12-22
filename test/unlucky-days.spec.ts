31d7a3b778e2512167d6d0cebed3f7a29ab426a5
alexkaufman06 <alexkaufman06@gmail.com>
2020-12-21 20:20:56 -0800

    Improve the spec description for unlucky days

--- test/unlucky-days.spec.ts
+++ test/unlucky-days.spec.ts
@@ -1,7 +1,7 @@
 import { unluckyDays } from "../src/unlucky-days";
 
 describe('Unlucky Days', () => {
-    it('Should return the number of Black Fridays for that year', function () {
+    it('Should return the number of Friday the 13ths for that year', function () {
         expect(unluckyDays(2015)).toEqual(3);
         expect(unluckyDays(1986)).toEqual(1);
     });
