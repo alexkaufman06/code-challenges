c6943e137dc15b28d1cfcf6c59a25230b78e718c
alexkaufman06 <alexkaufman06@gmail.com>
2021-06-13 14:19:39 -0700

    Allows builder to return basic major scales

--- src/scale-builder.ts
+++ src/scale-builder.ts
@@ -3,39 +3,57 @@ type sharp = '#';
 type flat = 'b';
 type natural = '';
 type accidentalOptions = sharp | flat | natural;
-type accidentals = '#' | 'b' | 'x' | 'bb' | '';
+// type accidentals = '#' | 'b' | 'x' | 'bb' | '';
 const sharpTones = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
 const flatTones = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab'];
+const sharpKeys = ['A', 'C', 'D', 'E', 'F#', 'G']; // C is included here even though it has no sharps/flats
+const flatKeys = ['B', 'Bb', 'F'];
 const majorScalePattern = ['1P', '2M', '3M', '4P', '5P', '6M', '7M', '8P'];
-const intervalValues = {
+const intervalValues: {[key: string]: number} = {
     '1P': 0,
     '2m': 1,
     '2M': 2,
     '3m': 3,
-    '4P': 4,
-    '5d': 5,
-    '5P': 6,
-    '6m': 7,
-    '6M': 8,
-    '7m': 9,
-    '7M': 10,
-    '8P': 11
+    '3M': 4,
+    '4P': 5,
+    '5d': 6,
+    '5P': 7,
+    '6m': 8,
+    '6M': 9,
+    '7m': 10,
+    '7M': 11,
+    '8P': 12
 };
 
-export function scaleBuilder(noteName: noteNames, accidental?: accidentalOptions): string[] {
-    if (!accidental) accidental = '';
-    const scale = [`${noteName}${accidental}`];
-    return scale;
+export class ScaleBuilder {
+    static majorScale(noteName: noteNames, accidental?: accidentalOptions): string[] {
+        let majorScale: string[] = [];
+        if (!accidental) accidental = '';
+        const note = noteName + accidental;
+        majorScalePattern.forEach(interval => {
+            majorScale.push(
+                getNoteFromInterval(note, interval)
+            );
+        });
+        return majorScale;
+    }
 }
 
-export function intervalCalculator(note: string, interval: string): string {
-    switch(interval) {
-        case '1P':
-            return note;
-            break;
-        case '2M':
-            return '';
-            break
+export function getNoteFromInterval(note: string, interval: string): string {
+    const isSharpKey = sharpKeys.includes(note);
+    if (isSharpKey) {
+        const position = sharpTones.indexOf(note);
+        let intervalPosition = position + intervalValues[interval];
+        if (intervalPosition > sharpTones.length - 1) {
+            intervalPosition -= sharpTones.length;
+        }
+        return sharpTones[intervalPosition];
+    } else {
+        const position = flatTones.indexOf(note);
+        let intervalPosition = position + intervalValues[interval];
+        if (intervalPosition > flatTones.length -1) {
+            intervalPosition -= flatTones.length;
+        }
+        return flatTones[intervalPosition];
     }
-    return '';
-}
+};
--- test/scale-builder.spec.ts
+++ test/scale-builder.spec.ts
@@ -1,19 +1,37 @@
-import { scaleBuilder } from '../src/scale-builder';
+import { ScaleBuilder } from '../src/scale-builder';
 
 describe('Scale Builder Tests', () => {
     it('returns an array', function () {
-        expect(Array.isArray( scaleBuilder('A'))).toEqual(true);
+        expect(Array.isArray(ScaleBuilder.majorScale('A'))).toEqual(true);
     });
 
-    it('returns the root note as the first note in the array', function () {
-        expect(scaleBuilder('A')[0]).toEqual('A');
+    it('returns the natural root note as the first note in the array', function () {
+        expect(ScaleBuilder.majorScale('A')[0]).toEqual('A');
     });
 
     it('returns the flatted root note as the first note in the array', function () {
-       expect(scaleBuilder('B', 'b')[0]).toEqual('Bb');
+        expect(ScaleBuilder.majorScale('B', 'b')[0]).toEqual('Bb');
     });
 
     it('returns the sharp root note as the first note in the array', function () {
-        expect(scaleBuilder('F', '#')[0]).toEqual('F#');
+        expect(ScaleBuilder.majorScale('F', '#')[0]).toEqual('F#');
+    });
+
+    it('returns the C major scale', function () {
+        expect(ScaleBuilder.majorScale('C')).toEqual([
+            'C', 'D', 'E', 'F', 'G', 'A', 'B', 'C'
+        ]);
+    });
+
+    it('returns the G major scale', function () {
+        expect(ScaleBuilder.majorScale('G')).toEqual([
+           'G', 'A', 'B', 'C', 'D', 'E', 'F#', 'G'
+        ]);
+    });
+
+    it('returns the D major scale', function () {
+        expect(ScaleBuilder.majorScale('D')).toEqual([
+            'D', 'E', 'F#', 'G', 'A', 'B', 'C#', 'D'
+        ]);
     });
 });
